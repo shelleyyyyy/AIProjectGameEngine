b025d3cc92b81d7bf35afa4b772a123600c25719
Cole Corson <corsonca23@mail.vmi.edu>
2022-09-30 10:43:24 -0400

    enviorment updates

--- Environment.py
+++ Environment.py
@@ -1,8 +1,8 @@
+from Location import Location
+from Cell import Cell
+
 class enviroment:
     width, height = (10, 10)
-    num_of_agents = 0
-    num_of_goals = 0
-    num_of_non_passable = 0
 
     def __init__(self, num_of_agents = 0, num_of_goals = 0, num_of_non_passable = 0):
         self.num_of_agents = num_of_agents
@@ -14,7 +14,14 @@ class enviroment:
         print(env)
         print(self.num_of_agents)
     
-
+    #returns an array of TruckAgents
+    def fill_cells(num_of_agents, num_obstacles):
+        for i in range(len(grid_size)):
+            for j in range(len(grid_size[i])):
+                location = Location()
+                cell = Cell(location=Location(i, j), width=width_of_cell, height=hieght_of_cell, 
+                percept=BLANK, direction=Constants.FACING_WEST)
+                #cell() create a cell. need cell class
 
 
 if __name__=="__main__":
